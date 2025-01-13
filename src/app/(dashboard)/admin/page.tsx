4865bb450850c1d277dbfb81b93320c02b608404
maksof-sharjeel <149464502+maksof-sharjeel@users.noreply.github.com>
2025-01-13 13:23:13 +0500

    session work

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,10 @@
+"use client"
 import React from 'react';
+import {useSession} from 'next-auth/react'
 
 const page = () => {
+	const session=  useSession()
+	console.log(session,"session")
 	return <div>Welcome to admin</div>;
 };
 
