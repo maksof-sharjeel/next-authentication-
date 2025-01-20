a9fdf8392532bb2e510be54b49eb5c854794e454
maksof-sharjeel <149464502+maksof-sharjeel@users.noreply.github.com>
2025-01-20 17:38:31 +0500

    work

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,21 +4,22 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const sessionToken = request.cookies.get('authjs.session-token')?.value;
   console.log(sessionToken, "sessionToken");
+
   const url = request.nextUrl.clone();
   if (!sessionToken) {
-    if (request.nextUrl.pathname !== '/sign-in') {
+    if (request.nextUrl.pathname !== '/sign-in' && request.nextUrl.pathname !== '/sign-up') {
       url.pathname = '/sign-in';
       url.search = '';
       return NextResponse.redirect(url);
     }
   }
-  if (sessionToken && request.nextUrl.pathname === '/sign-in') {
+  if (sessionToken && (request.nextUrl.pathname === '/sign-in' || request.nextUrl.pathname === '/sign-up')) {
     url.pathname = '/admin';
     return NextResponse.redirect(url);
   }
-
   return NextResponse.next();
 }
+
 export const config = {
-  matcher: ['/admin/:path*', '/sign-in'],
+  matcher: ['/admin/:path*', '/sign-in', '/sign-up'], 
 };
